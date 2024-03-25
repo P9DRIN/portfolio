4a5e5d09314d1842fb1955f1f873137552e80cae
Lima Silva <pedrohenriquelimasilva2837@hotmail.com>
2024-03-25 15:51:47 -0300

    Edit: previous experience edit

--- src/components/Experience/index.tsx
+++ src/components/Experience/index.tsx
@@ -5,8 +5,12 @@ export function Experience(){
         <>
         <h1 className="text-neutral-100 font-semibold ml-5 mt-1 md:text-xl lg:text-2xl">Experiência:</h1>
         <div className="text-neutral-300 flex flex-col items-center justify-center text-sm border-b border-neutral-600 p-2 md:text-base lg:text-lg">
-            <span>Designer Freelance - Photoshop & Illustrator</span>
-            <span>04/2022 - Atualmente - Jovem Aprendiz: Aux Adm. | Raizen</span>
+            <span><b>Designer Freelance - Photoshop & Illustrator:</b> Criação de artes visualmente atrativas, geração de leads através de criativos, postagens para social media, thumbnails, etc.</span>
+            <span> <b>Desenvolvedor Freelance:</b> Desenvolvimento de aplicações responsivas com uso de
+Tailwind/Styled-components utilizando a biblioteca React juntamente
+do NodeJS como API para o banco MongoDB serveless, permitindo
+escalabilidade, segurança e eficiência para a aplicação web.
+</span>
         </div>
         </>
     )
